bf3c2e2194c065e7bfb2878705d7d33a99f5cd4f
riengi <112477297+riengi@users.noreply.github.com>
2022-10-08 12:46:43 +0200

    Minor improvements of spl_token code

--- spl_token/src/main.rs
+++ spl_token/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
     // .env (get account_pk and token_id)
     let account = dotenv::var("account_pk").unwrap();
     let token_id = dotenv::var("token_id").unwrap();
+    println!("Account address:\t {}", account);
+    println!("Token ID:\t\t {}", token_id);
 
     // convert it to Pubkey
     let account_pk: Pubkey = account.parse().unwrap();
@@ -14,17 +16,21 @@ fn main() {
 
     // Initialize RPC client
     let url = dotenv::var("rpc_url").expect("rpc_url not found");
-    println!("{}", url);
+    println!("Network RPC url:\t {}", url);
     let client = RpcClient::new(url);
 
     // Get associated token address
     let associated_token_address =
         spl_associated_token_account::get_associated_token_address(&account_pk, &token_id_pk);
-    println!("Token account address: {:?}", associated_token_address);
+    println!("Token account address:\t {:?}", associated_token_address);
 
     // Get balance of associated token account
     let balance = client
         .get_token_account_balance(&associated_token_address)
         .unwrap();
-    println!("Token amount: {:?}", balance);
+    println!("Token amount:\t\t {:?}", balance);
+    if let Some(amount) = balance.ui_amount {
+        println!("SPL Token balance:\t {} tokens", amount)
+    }
+    println!("Token balance decimals:\t {}", balance.decimals);
 }
